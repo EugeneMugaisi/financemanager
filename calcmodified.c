5c6687a1016937f7b76760db44b032c6d2bc76dc
Eugene Mugaisi <Eugene@Eugenes-MacBook-Pro.local>
2024-02-22 23:13:10 +0300

    added modulo division

--- calcmodified.c
+++ calcmodified.c
@@ -1,7 +1,7 @@
 // ENE212-0083/2020
 // Mugaisi Eugene Muruli
 #include <stdio.h>
-
+#include <math.h>
 double add(double num1, double num2) {
     return num1 + num2;
 }
@@ -23,8 +23,17 @@ double divide(double num1, double num2) {
     }
 }
 
+double moduloDivision(double num1, double num2) {
+    if (num2 != 0) {
+        return fmod(num1, num2);
+    } else {
+        printf("Error: Modulo division by zero is undefined.\n");
+        return 0.0;
+    }
+}
+
 int main() {
-    double num1, num2, sum, difference, product, quotient, remainderValue;
+    double num1, num2, sum, difference, product, quotient, moduloResult;
     printf("\n\t Welcome to Eugene's simple Calculator\n");
     printf("Enter the first number: ");
     scanf("%lf", &num1);
@@ -35,6 +44,7 @@ int main() {
     difference = num1 - num2;
     product = num1 * num2;
     quotient = num1 / num2;
+    moduloResult = moduloDivision(num1, num2);
 
 
     printf("The sum of the numbers is: %lf\n", sum);
@@ -44,6 +54,8 @@ int main() {
     if (num2 != 0) {
         printf("The quotient of the numbers is: %.2lf\n", quotient);
     }
+    printf("The result of modulo division is: %.2lf\n", moduloResult);
+
 
    printf("\n\nThank you for using Eugene's calculator!");
     return 0;
